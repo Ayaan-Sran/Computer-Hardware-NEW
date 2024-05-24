be5936bdd0fee422ef091acdec6f52d235e644d1
Ayaan-Sran <160176550+Ayaan-Sran@users.noreply.github.com>
2024-05-24 14:24:19 -0700

    Final

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // script.js
 function calculateTotal() {
-    var cpuPrice = parseInt(document.getElementById("cpu").value);
-    var gpuPrice = parseInt(document.getElementById("gpu").value);
-    var ramPrice = parseInt(document.getElementById("ram").value);
-  
-    var total = cpuPrice + gpuPrice + ramPrice;
-  
-    document.getElementById("totalCost").textContent = "$" + total;
-  }
+  var cpuPrice = parseInt(document.getElementById("cpu").value);
+  var cpuCoolerPrice = parseInt(document.getElementById("cpuCooler").value);
+  var ssdPrice = parseInt(document.getElementById("ssd").value);
+  var pcCasePrice = parseInt(document.getElementById("pcCase").value);
+  var gpuPrice = parseInt(document.getElementById("gpu").value);
+  var psuPrice = parseInt(document.getElementById("psu").value);
+  var motherboardPrice = parseInt(document.getElementById("motherboard").value);
+  var ramPrice = parseInt(document.getElementById("ram").value);
+  var platingPrice = parseInt(document.getElementById("plating").value);
+
+  var total = cpuPrice + cpuCoolerPrice + ssdPrice + pcCasePrice + gpuPrice + psuPrice + motherboardPrice + ramPrice + platingPrice;
+
+  document.getElementById("totalCost").textContent = "$" + total;
+}
